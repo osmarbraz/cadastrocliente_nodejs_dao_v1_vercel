d4bee7718bb644551a00616e47453fb1e5f8c45e
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2026-09-13 09:41:27 -0300

    chore: teste conexão

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -2,29 +2,30 @@
  * Configurações e conexão com o banco de dados.
  */
 
-// Importa a biblioteca dotenv, responsável por carregar
-// as variáveis de ambiente armazenadas no arquivo .env
-const dotenv = require('dotenv');
-
-// Carrega as variáveis do arquivo .env para o objeto process.env
-dotenv.config();
-
-// Essa função permite estabelecer a conexão com um banco
-// de dados PostgreSQL hospedado no Neon.
-const { neon } = require('@neondatabase/serverless');
-
-// Cria a conexão com o banco de dados utilizando a string
-// de conexão armazenada na variável de ambiente DATABASE_URL.
-const sql = neon(process.env.DATABASE_URL);
-
 /**
  * Cria uma conexão com o banco de dados.
  * @returns 
  */
-async function createDbConnection() {
+function createDbConnection() {
+
+  // Importa a biblioteca dotenv, responsável por carregar
+  // as variáveis de ambiente armazenadas no arquivo .env
+  const dotenv = require('dotenv');
+
+  // Carrega as variáveis do arquivo .env para o objeto process.env
+  dotenv.config();
+
+  // Essa função permite estabelecer a conexão com um banco
+  // de dados PostgreSQL hospedado no Neon.
+  const { neon } = require('@neondatabase/serverless');
+
+  // Cria a conexão com o banco de dados utilizando a string
+  // de conexão armazenada na variável de ambiente DATABASE_URL.
+  const sql = neon(process.env.DATABASE_URL);
     
   console.log("Conexão com PostgreSQL foi estabelecida");
 
+  //Cria a tabela do banco de dados.
   createTable(sql);  
 
   return sql;
@@ -37,18 +38,16 @@ async function createDbConnection() {
  */
 async function createTable(db) {
      try {
-      await db`CREATE TABLE cliente (
+      await db`CREATE TABLE IF NOT EXISTS cliente (
             clienteId INTEGER, 
             nome VARCHAR(100), 
             cpf VARCHAR(11), 
             CONSTRAINT pk_cliente PRIMARY KEY (clienteId));`;
       console.log("Tabela cliente criada");
     } catch (error) {
-      console.log("Tabela já existe");
+      console.log("Tabela cliente já existe");
     }
   }
 
 // Inicializa a conexão e criação da tabela
-createDbConnection();  
-
-module.exports = sql;
+module.exports = createDbConnection(); 
